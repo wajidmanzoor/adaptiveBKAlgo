dbaa0d5ae25671e753c138a5c668abe731d2c012
wajid <wajid@wajids-MacBook-Air-2.local>
2025-10-14 12:32:26 -0400

    testing pivot BK

--- orginal/main.cpp
+++ orginal/main.cpp
@@ -14,11 +14,20 @@ int main(int argc, const char *argv[]) {
   Graph g(filepath);
 
   if (mode == 0) {
+    cout << "Running Adjacency Matrix Bron-Kerbosch..." << endl;
     AdjMatBK adjMatBk(g);
     adjMatBk.findAllMaximalCliques();
-  } else {
+  } else if (mode == 1) {
+    cout << "Running Adjacency List Bron-Kerbosch..." << endl;
     AdjListBK adjListBk(g);
     adjListBk.findAllMaximalCliques();
+  } else if (mode == 2) {
+    cout << "Running Pivot Bron-Kerbosch with pruning..." << endl;
+    PivotBK pivotBk(g);
+    pivotBk.findAllMaximalCliques();
+  } else {
+    cout << "Invalid mode! Use 0, 1, or 2." << endl;
+    exit(1);
   }
 
   return 0;
